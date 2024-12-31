5641e99321515a930409f3834271eae230a7b287
Iman Heidari <iman.heydari@gmail.com>
2024-12-31 13:54:24 -0800

    move data to json format

--- src/services/businesses-data.ts
+++ src/services/businesses-data.ts
@@ -1,70 +1,8 @@
 import { Business } from "./types";
+import data from "./businesses-data.json";
 
-export const businesses: Business[] = [
-  {
-    id: "iman-florist1",
-    name: "Iman Florist",
-    socialMedia: {
-      instagram: {
-        id: "",
-        name: "",
-        followers: 0,
-        posts: 0,
-        businessFollowersId: [],
-        isVerified: false,
-      },
-    },
-    category: {
-      id: "florist",
-      name: "Florist",
-    },
-    createdAt: new Date(),
-    updatedAt: new Date(),
-    serviceLocations: [],
-    tags: [],
-  },
-  {
-    id: "iman-florist2",
-    name: "Iman Florist",
-    socialMedia: {
-      instagram: {
-        id: "",
-        name: "",
-        followers: 0,
-        posts: 0,
-        businessFollowersId: [],
-        isVerified: false,
-      },
-    },
-    category: {
-      id: "florist",
-      name: "Florist",
-    },
-    createdAt: new Date(),
-    updatedAt: new Date(),
-    serviceLocations: [],
-    tags: [],
-  },
-  {
-    id: "iman-florist3",
-    name: "Iman Florist",
-    socialMedia: {
-      instagram: {
-        id: "",
-        name: "",
-        followers: 0,
-        posts: 0,
-        businessFollowersId: [],
-        isVerified: false,
-      },
-    },
-    category: {
-      id: "rental-decoration",
-      name: "Rental decoration",
-    },
-    createdAt: new Date(),
-    updatedAt: new Date(),
-    serviceLocations: [],
-    tags: [],
-  },
-];
+export const businesses: Business[] = data.map((business) => ({
+  ...business,
+  createdAt: new Date(),
+  updatedAt: new Date(),
+}));
