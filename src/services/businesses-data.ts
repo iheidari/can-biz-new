581a1ec3d577ff8f8a3a16f822fbf5da44305eea
Ehsan_Heidari <ehsan.heydari@gmail.com>
2025-01-02 22:51:24 -0800

    solved the build issue

--- src/services/businesses-data.ts
+++ src/services/businesses-data.ts
@@ -5,4 +5,11 @@ export const businesses: Business[] = data.map((business) => ({
   ...business,
   createdAt: new Date(),
   updatedAt: new Date(),
+  socialMedia: {
+    ...business.socialMedia,
+    instagram: {
+      ...business.socialMedia.instagram,
+      followers: Number(business.socialMedia.instagram.followers), // Convert followers to number
+    },
+  },
 }));
